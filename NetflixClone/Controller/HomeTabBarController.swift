42b9fc71f7f70c1e7bf57746d8dbaef6c1f27cdc
Sanyukta Adhate <sanyuktasudhir1417@gmail.com>
2026-02-11 14:27:35 +0530

    Initial commit

---
+++ NetflixClone/Controller/HomeTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeTabBarController.swift
+//  NetflixClone
+//
+//  Created by Sanyukta Adhate on 11/02/26.
+//
+
+import UIKit
+
+class HomeTabBarController: UITabBarController {
+    
+    @IBOutlet weak var homeTabBar: UITabBar!
+    
+    @IBOutlet weak var homeTab: UITabBarItem!
+    
+    
+    @IBOutlet weak var myNetflixTab: UITabBarItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NetflixClone/Controller/MyNetflixViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MyNetflixViewController.swift
+//  NetflixClone
+//
+//  Created by Sanyukta Adhate on 11/02/26.
+//
+
+import UIKit
+
+class MyNetflixViewController: UIViewController {
+    
+    
+    @IBAction func signInTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func signUpTapped(_ sender: UIButton) {
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
